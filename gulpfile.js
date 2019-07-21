3a3b587fc7f2ed0245564a423eaba5d2c4614b7d
Okiki <femikiki@gmail.com>
2019-07-21 19:34:45 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -258,8 +258,7 @@ task('inline', () =>
 );
 
 // Gulp task to minify all files
-task('default', series("update", "config", "server", "html", "css", "js"/*,
-"inline"*/));
+task('default', series("update", "config", "server", "html", "css", "js", "inline"));
 
 // Gulp task to check to make sure a file has changed before minify that file files
 task('watch', () => {
