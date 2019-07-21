82f47bea6db172c9acf896ce599ead9dbc5bdf73
Okiki <femikiki@gmail.com>
2019-07-21 21:43:31 +0000

    Faster

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
+/*
+// Feature detection
+// https://github.com/WICG/EventListenerOptions/blob/gh-pages/EventListenerOptions.polyfill.js
+var passiveIfSupported = false;
+
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, "passive", { get: function() { passiveIfSupported = { passive: true }; } }));
+} catch(err) {}
+
+window.addEventListener('scroll', function(event) {
+  // do something
+  // can't use event.preventDefault();
+}, passiveIfSupported ); */
+
+
+
 import el from "./components/ele";
 let ele = new el("body");
 ele.set("style", { });
