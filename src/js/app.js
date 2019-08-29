b15933b3c005ff9b05e139eb2df9cbf8a342ae14
Okiki <femikiki@gmail.com>
2019-08-29 21:25:56 +0000

    Mobile Done

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import swup from "swup";
 import el from "./components/ele";
 import { _log } from "./components/util";
-import preload from '@swup/preload-plugin';
 
+// import preload from '@swup/preload-plugin';
 // import './components/smoothstate';
 // import _class from "./components/class";
 // import _event from "./components/event";
@@ -19,24 +19,19 @@ _navbar.click('.navbar-menu', function(e) {
     _navbar.toggleClass("navbar-show");
 });
 
-_navbar.click('.navbar-menu', e => {
-    e.preventDefault();
-    _navbar.toggleClass("navbar-show");
-});
-
 _global.scroll(() => {
     _navbar.toggleClass("navbar-focus", (_global.scrollTop() + _height) >= _focusPt);
     _navbar.hasClass("navbar-show") && _navbar.removeClass("navbar-show");
 });
 
 if (window._isModern) {
-    let trans = new swup({
+    let trans = new swup(/*{
         requestHeaders: {
             "X-Requested-With": "swup", // So we can tell request comes from swup
             "x-partial": "swup" // Request a partial html page
         },
-        plugins: [new preload()]
-    });
+        // plugins: [new preload()]
+    }*/);
 
 
     // let _load = () => { };
